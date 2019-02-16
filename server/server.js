a68578832e05ebe0aa98a9b7a30c2fdbafdf0725
ultravisionoid <ultravisionoid@users.noreply.github.com>
2019-02-16 23:57:01 +0530

    add get/todos route

--- server/server.js
+++ server/server.js
@@ -23,8 +23,19 @@ app.post("/todos",(req,res)=>{
 		res.status(400).send(e);
 	});
 });
+
+app.get("/todos",(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	},(e)=>{
+		res.status(400).send(e);
+	})
+});;
+
+
 app.listen(3000,()=>{
 	console.log("started on 3000 port");
 })
 
 module.exports={app};
+
