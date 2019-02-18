ea209629447185ff0ea7ce89ae92c1d3d827ffc2
ultravisionoid <ultravisionoid@users.noreply.github.com>
2019-02-18 06:48:54 +0530

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var {ObjectID}=require("mongodb");
 
 var  {mongoose}=require("./db/mongoose");
 var {Todo}=require('./db/models/todo');
@@ -31,7 +31,21 @@ app.get("/todos",(req,res)=>{
 		res.status(400).send(e);
 	})
 });;
+app.get("/todos/:id",(req,res)=>{
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send("Not found1");
+	}
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send("Not found2");
+		}
+		res.status(200).send({todo});
+	}).catch((e)=>{
+		res.status(404).send("Not found3");
+	})
 
+})
 
 app.listen(3000,()=>{
 	console.log("started on 3000 port");
