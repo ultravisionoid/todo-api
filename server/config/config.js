a728d131378c6cb14ed9dcbb9e3a1d4ebf7c8d97
ultravisionoid <ultravisionoid@users.noreply.github.com>
2019-03-10 12:02:28 +0530

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
-
 var env = process.env.NODE_ENV || "development";
-console.log("!!!!env   ***"+env)
 
-if(env ==="development"){
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI ="mongodb://localhost:27017/TodoApp"
-}else if(env ==="test"){
-	process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
+console.log("!!!!env***************"+env)
+if(env==='development'||env==="test"){
+	var config =require("./config.json");
+	var envconfig = config[env];
+	Object.keys(envconfig).forEach((key)=>{
+		process.env[key]=envconfig[key];
+	})	
+
 
 }
+
